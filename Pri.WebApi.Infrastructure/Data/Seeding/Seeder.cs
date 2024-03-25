eb87ac40f6d15f413630f58f69893cc274430895
Mileto Di Marco <mileto.di.marco@howest.be>
2024-03-25 19:09:29 +0100

    Add user seeding

--- Pri.WebApi.Infrastructure/Data/Seeding/Seeder.cs
+++ Pri.WebApi.Infrastructure/Data/Seeding/Seeder.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Pri.CleanArchitecture.Core.Entities;
+using Pri.WebApi.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -46,10 +48,43 @@ namespace Pri.CleanArchitecture.Infrastructure.Data.Seeding
             };
 
             //Identity seeding
+            //admin seeding
+            var admin = new ApplicationUser
+            {
+                Id = "1",
+                UserName = "admin@food.com",
+                Email = "admin@food.com",
+                NormalizedEmail = "ADMIN@FOOD.COM",
+                NormalizedUserName = "ADMIN@FOOD.COM",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Firstname = "Bart",
+                Lastname = "Soete",
+                DateOfBirth = DateTime.Now,
+            };
+            //user seeding
+            var user = new ApplicationUser
+            {
+                Id = "2",
+                UserName = "user@food.com",
+                Email = "user@food.com",
+                NormalizedEmail = "USER@FOOD.COM",
+                NormalizedUserName = "USER@FOOD.COM",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Firstname = "Mileto",
+                Lastname = "Di Marco",
+                DateOfBirth = DateTime.Now,
+            };
+            //password hash
+            IPasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            admin.PasswordHash = passwordHasher.HashPassword(admin, "Test123");
+            user.PasswordHash = passwordHasher.HashPassword(user, "Test123");
             modelBuilder.Entity<Category>().HasData(categories);
             modelBuilder.Entity<Product>().HasData(products);
             modelBuilder.Entity<Property>().HasData(properties);
             modelBuilder.Entity($"{nameof(Product)}{nameof(Property)}").HasData(productsProperties);
+            modelBuilder.Entity<ApplicationUser>().HasData(admin, user);
         }
     }
 }
---
+++ Pri.WebApi.Infrastructure/Migrations/20240325180542_IdentitySeeding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pri.CleanArchitecture.Infrastructure.Migrations
+{
+    public partial class IdentitySeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "DateOfBirth", "Email", "EmailConfirmed", "Firstname", "Lastname", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "1", 0, "982e69b8-9523-427b-91e6-1beda58f91b0", new DateTime(2024, 3, 25, 19, 5, 41, 728, DateTimeKind.Local).AddTicks(998), "admin@food.com", false, "Bart", "Soete", false, null, "ADMIN@FOOD.COM", "ADMIN@FOOD.COM", "AQAAAAEAACcQAAAAEN/bEyfg+7x/QYKsVB3t9A8EfnJN6WHdt+xtKW6UcKuI6WonInJHnKNla/rD281yyw==", null, false, "ee8759f9-33b0-4ec5-85a9-95b041c0d6b1", false, "admin@food.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "DateOfBirth", "Email", "EmailConfirmed", "Firstname", "Lastname", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "2", 0, "67e2d4d2-026c-4f1f-a19e-6f19568a30f1", new DateTime(2024, 3, 25, 19, 5, 41, 728, DateTimeKind.Local).AddTicks(1090), "user@food.com", false, "Mileto", "Di Marco", false, null, "USER@FOOD.COM", "USER@FOOD.COM", "AQAAAAEAACcQAAAAEMRFiEU07gWppl78DDJNMjRVlDy+Fn7qDqbCmbHZPw3w1e2aIfY7moBpNcr/Xpqj8Q==", null, false, "0eb2c091-f0db-4e33-b336-030e78547016", false, "user@food.com" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2");
+        }
+    }
+}
